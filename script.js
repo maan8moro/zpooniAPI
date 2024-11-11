c817e9c8a797fcca7fdb74b7e63d0e74e2414787
Maen Khaled <94805728+maan8moro@users.noreply.github.com>
2024-11-11 14:39:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ const { chromium } = require("playwright"); // Import Playwright
 const cors = require("cors");
 const app = express();
 
-// let htmlContent;
+let htmlContent;
 
-// // Enable CORS for all routes
-// app.use(cors());
+// Enable CORS for all routes
+app.use(cors());
 
-// (async () => {
-//     // Launch the browser
-//     const browser = await chromium.launch();
-//     const page = await browser.newPage();
-//     await page.goto("https://moro-store.zbooni.com", { waitUntil: "networkidle" });
+(async () => {
+    // Launch the browser
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto("https://moro-store.zbooni.com", { waitUntil: "networkidle" });
 
-//     // Get the HTML content of the page
-//     htmlContent = await page.content();
+    // Get the HTML content of the page
+    htmlContent = await page.content();
 
-//     // Close the browser
-//     await browser.close();
-// })();
+    // Close the browser
+    await browser.close();
+})();
 
 app.get("/", (req, res) => {
-    res.send("test");
-    // if (htmlContent) {
-    //     res.send(htmlContent);
-    // } else {
-    //     res.send("Content is not ready yet. Please try again shortly.");
-    // }
+    // res.send("test");
+    if (htmlContent) {
+        res.send(htmlContent);
+    } else {
+        res.send("Content is not ready yet. Please try again shortly.");
+    }
 });
 
 app.listen(4000, () => console.log("Listening on http://localhost:4000"));
